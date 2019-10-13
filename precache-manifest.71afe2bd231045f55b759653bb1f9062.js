0de24b16ea7c5e0f87584dbf8a82c5ae4717195c
devyani <devyani271196@gmail.com>
2019-10-13 12:10:12 +0530

    Updates

---
+++ precache-manifest.71afe2bd231045f55b759653bb1f9062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc27e8a2239c155f7d9545b1ef98ab5",
+    "url": "/devyaniwaghmare/done.git/index.html"
+  },
+  {
+    "revision": "e79dee947a5ea5646428",
+    "url": "/devyaniwaghmare/done.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ea389f1e49c6da3706ad",
+    "url": "/devyaniwaghmare/done.git/static/js/2.9f0b5de0.chunk.js"
+  },
+  {
+    "revision": "e79dee947a5ea5646428",
+    "url": "/devyaniwaghmare/done.git/static/js/main.adb96513.chunk.js"
+  },
+  {
+    "revision": "9d49470eada2fd92731c",
+    "url": "/devyaniwaghmare/done.git/static/js/runtime-main.c2cd2da3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/devyaniwaghmare/done.git/static/media/logo.25bf045c.svg"
+  }
+]);
